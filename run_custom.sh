53189fc4314572a168ba594d63a32c379183e9df
Siyuan Wang <v-siywang@microsoft.com>
2025-01-14 10:57:37 +0000

    update load wait time

--- run_custom.sh
+++ run_custom.sh
@@ -7,7 +7,7 @@ MODEL_PATH=${1:-"/mnt/longcontext/models/siyuan/llama3/llama-3.1-8B-instruct"}
 vllm serve $MODEL_PATH --api-key token-abc123 --tensor-parallel-size 4 --gpu-memory-utilization 0.95 --max_model_len 131072 --trust-remote-code &
 
 # Wait for the server to fully start
-sleep 30
+sleep 75
 
 # Run the prediction script with the specified model path
 python pred.py --model_path $MODEL_PATH
