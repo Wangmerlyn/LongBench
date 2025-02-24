902dbe88419a8173845800dd7d8b147b8dd4f2b5
Wang Siyuan <v-siywang@microsoft.com>
2025-02-24 14:26:35 +0000

    fix asnwer extract

--- pred.py
+++ pred.py
@@ -168,7 +168,7 @@ if __name__ == "__main__":
     parser.add_argument("--model", "-m", type=str, default="GLM-4-9B-Chat")
     parser.add_argument("--model_path", type=str, default=None)
     parser.add_argument("--cot", "-cot", action='store_true') # set to True if using COT
-    parser.add_argument("--cot_answer_extract", type=bool, action='store_true') # set to True if using COT answer extraction
+    parser.add_argument("--cot_answer_extract", action='store_true') # set to True if using COT answer extraction
     parser.add_argument("--no_context", "-nc", action='store_true') # set to True if using no context (directly measuring memorization)
     parser.add_argument("--rag", "-rag", type=int, default=0) # set to 0 if RAG is not used, otherwise set to N when using top-N retrieved context
     parser.add_argument("--n_proc", "-n", type=int, default=16)
