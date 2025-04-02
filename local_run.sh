1b337eccf621e44f3380aa5c10f8d0a34023d84e
Wang Siyuan <v-siywang@microsoft.com>
2025-04-02 06:20:40 +0000

    add local run script

--- local_run.sh
+++ local_run.sh
@@ -1,8 +1,11 @@
-cot_prompt_type="train_align"
+cot_prompt_type="default"
 model_paths=(
-    "/mnt/longcontext/models/siyuan/rl_ckpts/uk2_llama3_4k+1k_8k+2k_no_math_20ksamples_10epochs_end-step100"
-    "/mnt/longcontext/models/siyuan/rl_ckpts/uk2_llama3_4k+1k_8k+2k_no_math_20ksamples_10epochs_end-step50"
+    "/mnt/longcontext/models/siyuan/rl_ckpts/orzboxed_llama3_15steps_fix_8k_2k_math_hard_2wiki_mathqachoice_niah_sentence_fix_128bsz_40ksamples_10epochs_end-step15"
 )
+
+# get the path of the current script
+current_script_path=$(realpath "$0")
+current_script_folder=$(dirname "$current_script_path")
 
 # iterate over the model paths
 for model_path in "${model_paths[@]}"; do
@@ -12,10 +15,11 @@ for model_path in "${model_paths[@]}"; do
     mkdir -p "/mnt/longcontext/models/siyuan/test_code/LongBench-v2/logs"
     # run the command with the extracted model name
     bash run_custom.sh --model_path "$model_path" \
-        --save_dir ~/results \
+        --save_dir $current_script_folder/results \
         --temperature 0.6 \
         --is_cot true \
         --log_file "/mnt/longcontext/models/siyuan/test_code/LongBench-v2/logs/${model_name}.log" \
         --num_gpus 4 \
         --cot_prompt_type $cot_prompt_type 
 done
+
