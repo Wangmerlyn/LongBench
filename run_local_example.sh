b2a751a05ced5f3d51d0d95fa37379c144a03b62
Wang Siyuan <v-siywang@microsoft.com>
2025-04-19 13:00:21 +0000

    fix local example

--- run_local_example.sh
+++ run_local_example.sh
@@ -6,4 +6,4 @@ bash run_custom.sh \
     --cot_answer_extract true \
     --log_file logs/debug_llama3_8k_2k_math_hard_2wiki_mathqachoice_128bsz_30ksamples_10epochs_end-step140.log \
     --temperature 0.6 \
-    --save_dir ~/results
+    --save_dir ./results
